6c96a613a0e31af04717f024ecde056eed9919f7
Adityavardhan <adityavardhansaluja@gmail.com>
2026-06-09 13:24:03 +0530

    Data model Implementation(Made changes in entity)

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,10 +1,14 @@
 package com.wellsfargo.counselor.entity;
 
 
+import org.springframework.web.servlet.FlashMapManager;
+import java.time.LocalDate;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Advisor {
@@ -82,5 +86,231 @@ public class Advisor {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+}
+
+
+@Entity
+class Client{
+    @Id
+    @GeneratedValue
+    private long Clientid;
+
+    @ManyToOne
+    @JoinColumn(name="advisorID")
+    private Advisor advisor;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String email;
+    
+    protected Client(String firstName, String lastName, String address, String phone, String email){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.phone=phone;
+        this.email=email;
+    }
+
+    public long getClientId(){
+        return Clientid;
+    }
+
+    public Advisor getAdvisor(){
+        return advisor;
+    }
+
+    public String getfirstName(){
+        return firstName;
+    }
+
+    public void setfirstName(String firstName){
+        this.firstName=firstName;
+    }
+
+    public String getlastName(){
+        return lastName;
+    }
+
+    public void setlastName(String lastName){
+        this.lastName=lastName;
+
+    }
+
+    public String getaddress(){
+        return address;
+    }
+
+    public void setaddress(String address){
+        this.address=address;
+    }
+
+    public String getphone(){
+        return phone;
+    }
+
+    public void setphone(String phone){
+        this.phone=phone;
+    }
+
+    public String getemail(){
+        return email;
+    }
+
+    public void setemail(String email){
+        this.email=email;
+    }
+
+}
+
+@Entity
+class Portfolio{
+    @Id
+    @GeneratedValue
+    private long PortfolioID;
+
+    @ManyToOne
+    @JoinColumn(name="clientId")
+    private Client client;
+
+    @Column(nullable = false)
+    private LocalDate creationDate;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(Client client, LocalDate creationDate) {
+        this.client = client;
+        this.creationDate = creationDate;
+    }
+
+    public long getPortfolioID() {
+        return PortfolioID;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+}
+
+@Entity
+class Security {
+
+    @Id
+    @GeneratedValue
+    private long securityId;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolioId")
+    private Portfolio portfolio;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private double purchasePrice;
+
+    @Column(nullable = false)
+    private LocalDate purchaseDate;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    protected Security() {
+
+    }
+
+    public Security(Portfolio portfolio,
+                    String name,
+                    String category,
+                    double purchasePrice,
+                    LocalDate purchaseDate,
+                    int quantity) {
+
+        this.portfolio = portfolio;
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+    }
+
+    public long getSecurityId() {
+        return securityId;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
